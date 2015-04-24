c482e34e9a778c4732478b678339b2c8461fe018
Donatien Garnier <donatien.garnier@appnearme.com>
2015-04-24 15:08:10 +0100

    Added Apache 2 headers

--- src/transport.h
+++ src/transport.h
@@ -15,6 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  */
+
 #ifndef SRC_TRANSPORT_H_
 #define SRC_TRANSPORT_H_
 
